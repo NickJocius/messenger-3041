607d7313207d028f266c544221f794a48d1ac247
Nick Jocius <capacity512@yahoo.com>
2021-10-28 10:22:16 -0500

    positioned bgimg of sidebanner and overlay color

--- client/src/components/Login/SideBanner.js
+++ client/src/components/Login/SideBanner.js
@@ -6,7 +6,13 @@ import bgImg from "../../assets/images/bg/bg-img.png"
 const useStyles = makeStyles(() => ({
     root: {
         minWidth: '100%',
-        background: `center url(${bgImg})`
+        background: `center / cover no-repeat url(${bgImg})`,
+    },
+    overlay: {
+        width: '100%',
+        height: '100%',
+        background: 'linear-gradient(45deg, rgba(57,141,255,0.85) , rgba(134,185,255,0.85) )'
+        
     }
   }));
 
@@ -14,7 +20,7 @@ const SideBanner = () => {
     const classes = useStyles();
     return (
         <Box className={classes.root}>
-            hello
+            <Box className={classes.overlay}></Box>
         </Box>
     );
 };
